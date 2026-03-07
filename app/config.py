59f715f3c6c321df6132e73eb287094feae75cc0
bogdanicduska <bogdanic.duska@gmail.com>
2026-03-07 17:25:23 +0100

    Remove TMDB API key from code

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT", "bigquery-movie-search")
 BQ_DATASET = os.getenv("BQ_DATASET", "movies_app")
-TMDB_API_KEY = os.getenv("TMDB_API_KEY", "ec42ba05704c101ff6429b228082b629")
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
